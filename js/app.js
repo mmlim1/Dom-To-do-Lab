a7e99e1b66270f0fab3f94f2ee784f2f17011ca7
mmlim1 <michaelmlim1@gmail.com>
2022-06-01 13:04:53 -0700

    add event listener with function to button

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@ const inp = document.getElementById('input-box')
 const btn = document.getElementById('submit-button')
 const ul = document.getElementById('todo-list')
 
+btn.addEventListener('click', function(evt) {
+  const li = document.createElement('li')
+  li.textContent = inp.value
+  inp.value = ''
+  ul.appendChild(li)
+})
